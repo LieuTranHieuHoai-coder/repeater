8da177086e35afeb88d0157f9c541cae119a61fc
= <=>
2019-12-12 21:10:49 +0700

    repeater thêm xóa

---
+++ repeater_2/repeater_2/kiemtra.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="kiemtra.aspx.cs" Inherits="repeater_2.kiemtra" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="bootstrap4-offline-docs-master/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="bootstrap4-offline-docs-master/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Image ID="Image1" runat="server" />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Value="1">Active</asp:ListItem>
+                <asp:ListItem Value="0">Disable</asp:ListItem>
+            </asp:DropDownList>
+            <asp:FileUpload ID="FileUpload1" runat="server" /><br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Them" />
+            <asp:Button ID="btn_xoa" runat="server" OnClick="btn_xoa_Click" Text="Xoa" />
+            <asp:Button ID="btn_update" runat="server" OnClick="btn_update_Click" Text="Update" />
+            <br />
+            <br />
+
+            <br />
+            <asp:TextBox ID="txt_key" runat="server"></asp:TextBox>
+            <asp:Button ID="btn_tim" OnClick="btn_tim_Click" runat="server" Text="Search" />
+           
+            <table class="table table-bordered table-dark table-striped">
+                <tr>
+                    <td>ID</td>
+                    <td>Image</td>
+                    <td>Name</td>
+                    <td>Stt</td>
+                </tr>
+                <asp:Repeater ID="Repeater1" runat="server">
+                    <ItemTemplate>
+                        <tr>
+                            <td><%# Eval("id") %></td>
+                            <td>
+                                <img src="img/<%# Eval("img") %>" />
+                            </td>
+                            <td><%# Eval("Name") %></td>
+                            <td><%# Eval("stt").ToString()=="1"?"Active":"Disable" %></td>
+                            <td><a href="?id=<%# Eval("id") %>">Edit</a></td>
+                        </tr>
+                    </ItemTemplate>
+                </asp:Repeater>
+
+
+            </table>
+
+            <asp:Repeater ID="Repeater2" runat="server">
+
+                <ItemTemplate>
+                    <a href="?page=<%#Eval("index") %><%if (Request["id"] != null) Response.Write("&id=" + Request["id"]); %>"><%#Eval("index") %> </a>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ repeater_2/repeater_2/kiemtra.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+namespace repeater_2
+{
+    public partial class kiemtra : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-B1PJQ57\HIEUHOAI;Initial Catalog=Repeater;Integrated Security=True");
+            conn.Open();
+            string query = "select * from sp";
+            
+            if (Request["key"]!=null)
+            {
+                query = "select * from sp where name like '%"+ Request["key"].ToString() +"%'";
+            }
+            SqlDataAdapter da = new SqlDataAdapter(query,conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            int so_item_1_trang = 3;
+            int so_trang = (dt.Rows.Count / so_item_1_trang) + (dt.Rows.Count % so_item_1_trang == 0 ? 0 : 1);
+            int page = Request["page"] == null ? 1 : Convert.ToInt32(Request["page"].ToString());
+            int from = (page - 1) * 3;
+            int to = page * 3 - 1;
+            for (int i = dt.Rows.Count - 1; i >=0; i--)
+            {
+                if(i < from || i > to)
+                {
+                    dt.Rows.RemoveAt(i);
+                }
+            }
+            Repeater1.DataSource = dt;
+            Repeater1.DataBind();
+            conn.Close();
+            if (Request["id"]!=null)
+            {
+                query = "select * from sp where id = '" + Request["id"] + "'";
+                SqlDataAdapter list = new SqlDataAdapter(query, conn);
+                DataTable dtlist = new DataTable();
+                list.Fill(dtlist);
+                TextBox1.Text = dtlist.Rows[0]["name"].ToString();
+                Image1.Visible = true;
+                Image1.ImageUrl = "~/img/" + dtlist.Rows[0]["img"].ToString();
+                DropDownList1.SelectedValue = dtlist.Rows[0]["stt"].ToString();
+            }
+            DataTable dtPage = new DataTable();
+            dtPage.Columns.Add("index");
+            dtPage.Columns.Add("active");
+            for (int i = 1; i <= so_trang; i++)
+            {
+                DataRow dr = dtPage.NewRow();
+                dr["index"] = i;
+
+                dtPage.Rows.Add(dr);
+            }
+            Repeater2.DataSource = dtPage;
+            Repeater2.DataBind();
+        }
+
+        protected void btn_tim_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("kiemtra.aspx?key="+txt_key.Text);
+        }
+        //nút thêm
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-B1PJQ57\HIEUHOAI;Initial Catalog=Repeater;Integrated Security=True");
+                conn.Open();
+                
+                string unit = Path.GetExtension(FileUpload1.FileName);
+                if (unit==".jpg"||unit==".png")
+                {
+                    string path = Server.MapPath("img\\");
+                    string name = FileUpload1.FileName;
+                    FileUpload1.SaveAs(path + name);
+
+                    string insert = "insert into sp values('"+TextBox1.Text+"','" + name + "','"+DropDownList1.SelectedValue+"')";
+                    SqlCommand cmd = new SqlCommand(insert, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void btn_xoa_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-B1PJQ57\HIEUHOAI;Initial Catalog=Repeater;Integrated Security=True");
+            conn.Open();
+
+            string xoa = "update sp set stt = 0 where id = '" + Request["id"] + "'";
+            SqlCommand cmd = new SqlCommand(xoa, conn);
+            cmd.ExecuteNonQuery();
+            Response.Write("xoa thành công");
+        }
+
+        protected void btn_update_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
